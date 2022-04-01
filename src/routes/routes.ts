2b1691973bcc0bf8d8c31ab71b5754ce111821d9
Serje <dezhik@yandex.ru>
2022-04-01 22:18:36 +0300

    исправлена ошибка путей

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,6 +2,7 @@ import * as VueRouter from "vue-router";
 
 import ListPage from '../components/ListPage.vue'
 import SummaryPage from '../components/SummaryPage.vue'
+import NotFound from '../components/NotFound.vue'
 
 const routes = [
   {
@@ -13,10 +14,14 @@ const routes = [
     path: '/navigator', 
     component: ListPage,
   },
+  { 
+    path: '/:pathMatch(.*)*', 
+    component: NotFound,
+  },
 ]
 
 export const router = VueRouter.createRouter({
-  history: VueRouter.createWebHistory(),
+  history: VueRouter.createWebHistory('/transactions/'),
   routes,
 })
 
